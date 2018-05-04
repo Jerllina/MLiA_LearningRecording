fcdff28501d8868b3ee894b492cf3887c9a84856
Jelina <lijie201718@gmail.com>
2018-05-04 19:12:44 +0800

    kNN Digits Recognition, accu0.99

---
+++ kNN/kNN_Digits.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 16:08:22 2018
+
+@author: lijie
+"""
+
+from numpy import *
+import operator
+from os import listdir  #列出给定目录的文件名
+import time
+
+
+'''Format picture into vectors'''
+def img2vector(filename):
+    #picture size 32*32
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    hwLabels = []
+    #load the training set
+    trainingFileList = listdir('trainingDigits')           
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')        #iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print( "\nthe total number of errors is: %d" % errorCount)
+    print( "\nthe total error rate is: %f" % (errorCount/float(mTest)))
+
+'''single classifier'''
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()     
+    classCount={}          
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+   
+    
+if __name__ == '__main__':
+    start = time.clock()
+    #img2vector('testDigits/0_13.txt')
+    handwritingClassTest()
+    end = time.clock()
+    print('time used： %s' % str(end - start))
