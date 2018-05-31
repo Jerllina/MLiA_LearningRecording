3467ba678718f4c3889dafabbd1ef9da49769267
Jelina <lijie201718@gmail.com>
2018-05-31 20:36:51 +0800

    Simplified SMO

---
+++ SVM/svm_SMO.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 31 18:16:01 2018
+
+@author: lijie
+"""
+from numpy import *
+
+'''load data'''
+def loadDataSet(fileName):
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])#alpha1,alpha2
+        labelMat.append(float(lineArr[2]))#label
+    return dataMat,labelMat
+
+def selectJrand(i,m):#m:the number of alpha
+    j=i #select any J not equal to i
+    while (j==i):
+        j = int(random.uniform(0,m))
+    return j
+        
+#modify aj
+def clipAlpha(aj,H,L):
+    if aj > H: 
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+'''simplified SMO'''
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    #input:dataset,labels,C,tolerated error rate,max iter times
+    dataMatrix = mat(dataMatIn); labelMat = mat(classLabels).transpose()
+    b = 0; m,n = shape(dataMatrix)
+    alphas = mat(zeros((m,1)))#initialize to 0
+    iter = 0
+    while (iter < maxIter):
+        alphaPairsChanged = 0#record the alpha changed times
+        for i in range(m):
+            fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T)) + b
+            Ei = fXi - float(labelMat[i])#prediction error
+            if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i,m)
+                fXj = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[j,:].T)) + b
+                Ej = fXj - float(labelMat[j])
+                alphaIold = alphas[i].copy(); alphaJold = alphas[j].copy();
+                if (labelMat[i] != labelMat[j]):
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if L==H: print ("L==H"); continue
+                #eta:Optimal modifying quantity .
+                #if eta=0,exit,do not need modify
+                eta = 2.0 * dataMatrix[i,:]*dataMatrix[j,:].T - dataMatrix[i,:]*dataMatrix[i,:].T - dataMatrix[j,:]*dataMatrix[j,:].T
+                if eta >= 0: print ("eta>=0"); continue
+                alphas[j] -= labelMat[j]*(Ei - Ej)/eta
+                alphas[j] = clipAlpha(alphas[j],H,L)
+                if (abs(alphas[j] - alphaJold) < 0.00001): print("j not moving enough"); continue
+                alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])#update i by the same amount as j
+                                                                        #the update is in the oppostie direction
+                b1 = b - Ei- labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[i,:].T - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[i,:]*dataMatrix[j,:].T
+                b2 = b - Ej- labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[j,:].T - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[j,:]*dataMatrix[j,:].T
+                if (0 < alphas[i]) and (C > alphas[i]): b = b1
+                elif (0 < alphas[j]) and (C > alphas[j]): b = b2
+                else: b = (b1 + b2)/2.0
+                alphaPairsChanged += 1
+                print("iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
+        if (alphaPairsChanged == 0): iter += 1
+        else: iter = 0
+        print ("iteration number: %d" % iter)
+    return b,alphas
+
+
+if __name__=='__main__' :
+#test
+    dataArr, labelArr=loadDataSet('testSet.txt')
+    b,alphas=smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
+    print('b:',b)
+    print('alphas:',alphas[alphas>0])
