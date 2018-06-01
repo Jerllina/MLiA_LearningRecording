3c35761bee1feaf04da97ef93d534de78619acb8
Jelina <lijie201718@gmail.com>
2018-06-01 22:13:08 +0800

    completed SMO & a simple test classifier myself

---
+++ SVM/completed_SMO.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  1 16:43:27 2018
+
+@author: lijie
+"""
+from numpy import *
+from svm_SMO import *
+
+
+################################################################################
+#                    TOO TOO TOO TOO TOO TOO HARD (T T)
+################################################################################
+
+'''define a data structure to store all important values'''
+class optStruct:
+    def __init__(self,dataMatIn, classLabels, C, toler, kTup):  # Initialize the structure with the parameters 
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m,1)))
+        self.b = 0
+        self.eCache = mat(zeros((self.m,2))) #first column:valid flag
+        
+        self.K = mat(zeros((self.m,self.m)))
+        for i in range(self.m):
+            self.K[:,i] = kernelTrans(self.X, self.X[i,:], kTup)#kTup: store the information of the kernel
+
+'''simplify the whole algrithm as this part is repeated so many times'''            
+def calcEk(oS, k):
+    fXk = float(multiply(oS.alphas,oS.labelMat).T*oS.K[:,k] + oS.b)
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+
+''''''
+def selectJ(i, oS, Ei):         #this is the second choice -heurstic, and calcs Ej
+    maxK = -1; maxDeltaE = 0; Ej = 0
+    oS.eCache[i] = [1,Ei]  #set valid #choose the alpha that gives the maximum delta E
+    validEcacheList = nonzero(oS.eCache[:,0].A)[0] #filter nonzero alpha
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:   #loop through valid Ecache values and find the one that maximizes delta E
+            if k == i: 
+                continue 
+            Ek = calcEk(oS, k)
+            deltaE = abs(Ei - Ek)
+            if (deltaE > maxDeltaE):
+                maxK = k; maxDeltaE = deltaE; Ej = Ek
+        return maxK, Ej
+    else:   #1st time ,choose random j 
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+    return j, Ej
+
+def updateEk(oS, k): #after any alpha changed, update value in the cache
+    Ek = calcEk(oS, k)
+    oS.eCache[k] = [1,Ek]
+
+'''improved decision boundary searching method'''    
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j,Ej = selectJ(i, oS, Ei) # which is [selectJrand] function in simplified SMO
+        alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy();
+        if (oS.labelMat[i] != oS.labelMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L==H: print ("L==H"); return 0
+        eta = 2.0 * oS.K[i,j] - oS.K[i,i] - oS.K[j,j] #changed for kernel
+        if eta >= 0: print( "eta>=0"); return 0
+        oS.alphas[j] -= oS.labelMat[j]*(Ei - Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j],H,L)
+        updateEk(oS, j) # Ecache update
+        if (abs(oS.alphas[j] - alphaJold) < 0.00001): print( "j not moving enough"); return 0
+        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])#update i by the same amount as j
+        updateEk(oS, i) #Ecache update                   #the update is in the oppostie direction
+        b1 = oS.b - Ei- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.K[i,i] - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.K[i,j]
+        b2 = oS.b - Ej- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.K[i,j]- oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.K[j,j]
+        if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): oS.b = b1
+        elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
+        else: oS.b = (b1 + b2)/2.0
+        return 1
+    else: return 0
+    
+'''package all the functions above to complete the SMO''' 
+def smoP(dataMatIn, classLabels, C, toler, maxIter,kTup):    #full Platt SMO
+    oS = optStruct(mat(dataMatIn),mat(classLabels).transpose(),C,toler, kTup)
+    iter = 0
+    entireSet = True; alphaPairsChanged = 0
+    while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):# 1 time Loop → iter+1
+        alphaPairsChanged = 0
+        if entireSet:   #go over all
+            for i in range(oS.m):        
+                alphaPairsChanged += innerL(i,oS)
+                print ("fullSet, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
+            iter += 1
+        else:#go over non-bound  alphas
+            nonBoundIs = nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerL(i,oS)
+                print("non-bound, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
+            iter += 1
+        if entireSet: entireSet = False #toggle entire set loop
+        elif (alphaPairsChanged == 0): entireSet = True  
+        print ("iteration number: %d" % iter)
+    return oS.b,oS.alphas 
+
+'''kernel trans function'''
+def kernelTrans(X, A, kTup): #calc the kernel or transform data to a higher dimensional space
+    m,n = shape(X)
+    K = mat(zeros((m,1)))
+    if kTup[0]=='lin': 
+        K = X * A.T   #linear kernel
+    elif kTup[0]=='rbf':
+        for j in range(m):
+            deltaRow = X[j,:] - A
+            K[j] = deltaRow*deltaRow.T
+        K = exp(K/(-1*kTup[1]**2)) #divide in NumPy is element-wise different from matrix in Matlab  
+                                            #rbf kernel
+    else: raise NameError('Houston We Have a Problem -- \
+    That Kernel is not recognized')
+    return K 
+
+def calcWs(alphas,dataArr,classLabels):
+    X = mat(dataArr); labelMat = mat(classLabels).transpose()
+    m,n = shape(X)
+    w = zeros((n,1))
+    for i in range(m):
+        w += multiply(alphas[i]*labelMat[i],X[i,:].T)
+    return w
+
+'''I package a simple classifier using the alphas to test above myself here'''
+def classifier11(i,alphas,dataArr,classLabels):
+    ws=calcWs(alphas,dataArr,classLabels)
+    dataMat=mat(dataArr)
+    v=dataMat[i]*mat(ws)+b
+    if v>0:
+        pred=1.0
+        if pred==labelArr[0]:
+            print('classified to 1,right')
+        else:
+            print('classified to 1,wrong')
+    else:
+        pred=-1.0
+        if pred==labelArr[0]:
+            print('classified to -1,right')
+        else:
+            print('classified to -1,wrong')
+
+if __name__=='__main__' :
+#test
+    
+    dataArr, labelArr=svm_SMO.loadDataSet('testSet.txt')
+    b,alphas=smoP(dataArr, labelArr, 0.6, 0.001, 40,kTup=('lin', 0))
+    classifier11(1,alphas,dataArr,labelArr)
+
